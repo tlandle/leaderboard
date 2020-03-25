65a58670014c7e251e39f220690ace5da212c70c
bradyz <brady.zhou@utexas.edu>
2020-03-25 13:34:29 -0500

    add port

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -11,5 +11,6 @@ python ${LEADERBOARD_ROOT}/leaderboard/leaderboard_evaluator.py \
 --agent-config=${TEAM_CONFIG} \
 --debug=${DEBUG_CHALLENGE} \
 --record=${RECORD_PATH} \
---resume=${RESUME}
+--resume=${RESUME} \
+--port=${PORT}
 
